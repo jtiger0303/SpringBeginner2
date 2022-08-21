c46dfed537ae48ab097ba3e35591d106bb89ea26
Jae Hyeok Shin <jtiger0303@ajou.ac.kr>
2022-08-22 01:42:31 +0900

    Adding searching posts on board

--- ch4/src/test/java/com/fastcampus/ch4/domain/PageHandlerTest.java
+++ ch4/src/test/java/com/fastcampus/ch4/domain/PageHandlerTest.java
@@ -1,27 +1,28 @@
-package com.fastcampus.ch4.domain;
-
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
-public class PageHandlerTest {
-    @Test
-    public void test(){
-        PageHandler ph= new PageHandler(250, 1);
-        assertTrue(ph.getBeginPage() ==1);
-        assertTrue(ph.getEndPage() ==10);
-    }
-
-    @Test
-    public void test2(){
-        PageHandler ph= new PageHandler(250, 11);
-        assertTrue(ph.getBeginPage() ==11);
-        assertTrue(ph.getEndPage() ==20);
-    }
-    @Test
-    public void test3(){
-        PageHandler ph= new PageHandler(255, 10);
-        assertTrue(ph.getBeginPage() ==1);
-        assertTrue(ph.getEndPage() ==10);
-    }
-}
+//package com.fastcampus.ch4.domain;
+//
+//import com.fastcampus.ch4.domain.PageHandler;
+//import org.junit.Test;
+//
+//import static org.junit.Assert.*;
+//
+//public class PageHandlerTest {
+//    @Test
+//    public void test(){
+//        PageHandler ph= new PageHandler(250, 1);
+//        assertTrue(ph.getBeginPage() ==1);
+//        assertTrue(ph.getEndPage() ==10);
+//    }
+//
+//    @Test
+//    public void test2(){
+//        PageHandler ph= new PageHandler(250, 11);
+//        assertTrue(ph.getBeginPage() ==11);
+//        assertTrue(ph.getEndPage() ==20);
+//    }
+//    @Test
+//    public void test3(){
+//        PageHandler ph= new PageHandler(255, 10);
+//        assertTrue(ph.getBeginPage() ==1);
+//        assertTrue(ph.getEndPage() ==10);
+//    }
+//}
